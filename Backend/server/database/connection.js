3c33db971c43acb6555af53824307fe6435426aa
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-03-20 18:44:23 +0530

    db made local

--- Backend/server/database/connection.js
+++ Backend/server/database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
   try{
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  const conn = await mongoose.connect("mongodb://127.0.0.1:27017/empmng");
   console.log(`MongoDB connected: ${conn.connection.host}`);
   }
   catch(err){
